0fd6a6c6e5e44a136f6e93aa5966217ae89df6e7
Michael Storgaard <github@michaelstorgaard.com>
2013-07-02 10:09:03 -0700

    Added customer email as external ID (fixes #11), added product SKU as external ID (fixes #12), changed productNo to SKU (fixes #13)

--- app/code/community/BillysBilling/Invoicer/Model/Observer.php
+++ app/code/community/BillysBilling/Invoicer/Model/Observer.php
@@ -155,7 +155,7 @@ class BillysBilling_Invoicer_Model_Observer {
         $responseArray = array();
         $id = null;
         try {
-            $address = $type . "?q=" . urlencode($data['name']);
+            $address = $type . "?externalId=" . urlencode($data['externalId']);
             if ($this->testMode) {
                 $response = $this->client->fakeGet(Mage::getBaseDir() . "/tests/output.log", $address);
             } else {
@@ -209,6 +209,7 @@ class BillysBilling_Invoicer_Model_Observer {
                 'state' => $data->getRegion(),
                 'phone' => $data->getTelephone(),
                 'fax' => $data->getFax(),
+                'externalId' => $data->getEmail(),
                 'persons' => array(
                     array(
                         'name' => $data->getName(),
@@ -223,8 +224,8 @@ class BillysBilling_Invoicer_Model_Observer {
                 "accountId" => $this->accountId,
                 "vatModelId" => $this->vatModelId,
                 "productType" => "product",
-                "productNo" => $data->product_id,
-                "suppliersProductNo" => $data->sku,
+                "productNo" => $data->sku,
+                "externalId" => $data->sku,
                 "prices" => array(
                     array(
                         "currencyId" => Mage::app()->getStore()->getCurrentCurrencyCode(),
